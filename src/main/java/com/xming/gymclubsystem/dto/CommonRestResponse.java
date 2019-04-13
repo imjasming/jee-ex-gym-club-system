b1c117a3fbfd31ffd3cead92d60170178ac65a4e
AlgorithChang <1361372700@qq.com>
2019-04-13 22:50:31 +0800

    modify rest response entity

--- src/main/java/com/xming/gymclubsystem/dto/CommonRestResponse.java
+++ src/main/java/com/xming/gymclubsystem/dto/CommonRestResponse.java
@@ -14,7 +14,7 @@ public class CommonRestResponse implements Serializable {
     private static final long serialVersionUID = -8965948882721460674L;
     public static final int SUCCESS = 200;
     public static final int FAILED = 500;
-    public static final int VALIDATE_FAILED = 404;
+    public static final int VALIDATE_FAILED = 400;
     //未认证
     public static final int UNAUTHORIZED = 401;
     //未授权
@@ -27,6 +27,26 @@ public class CommonRestResponse implements Serializable {
         this.code = SUCCESS;
         this.message = "Success";
         this.data = data;
+        return this;
+    }
+
+    public CommonRestResponse failed() {
+        this.code = FAILED;
+        this.message = "server internal exception";
+        return this;
+    }
+
+    public CommonRestResponse unauthorized(String message) {
+        this.code = UNAUTHORIZED;
+        this.message = "unauthorized or token is expired";
+        this.data = message;
+        return this;
+    }
+
+    public CommonRestResponse forbidden(String message) {
+        this.code = FORBIDDEN;
+        this.message = "authentication failed";
+        this.data = message;
         return this;
     }
 }
